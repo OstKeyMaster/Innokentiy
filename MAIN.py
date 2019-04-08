d56b909036b133c504d2f91c3016786d244777da
NicoNii <ostkeymaster@yandex.ru>
2019-04-08 23:49:27 +0300

    Всё заново... "Привет", "Чуитс" и "Кто ты"

---
+++ MAIN.py
@@ -0,0 +1,48 @@
+import random
+import requests
+import vk_api
+from CONFIG import *
+
+
+def write_msg(usr_id, text):
+    vk_bot.method('messages.send', {'user_id': usr_id, 'message': text, 'random_id': random.randint(0, 1000)})
+
+
+def write_msg_attach(usr_id, text, att_url):
+    vk_bot.method('messages.send', {'user_id': usr_id,
+                                    'attachment': att_url,
+                                    'message': text,
+                                    'random_id': random.randint(0, 1000)})
+
+
+# START HERE<>><><><><><><><><><><><><<<<<<<<<<<<<<>>>>>>>>>>>>>>
+
+
+vk_bot = vk_api.VkApi(token=TOKEN)
+long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
+server, key, ts = long_poll['server'], long_poll['key'], long_poll["ts"]
+print("готов к работе " + str(long_poll))
+while True:
+    long_poll = requests.get(
+        'https://{server}?act={act}&key={key}&ts={ts}&wait=500'.format(
+            server=server, act='a_check', key=key, ts=ts)).json()
+
+    update = long_poll['updates']
+    print(update)
+    if update[0][0] == 4:
+        user_id = update[0][3]
+        usr_txt = update[0][6]
+
+        if 'Кто ты' in usr_txt or 'Ты кто' in usr_txt:
+            group_info = vk_bot.method('groups.getById', {'group_id': GROUP_ID, 'fields': 'name'})
+            write_msg(user_id, 'Я - ' + group_info[0]['name'] + ', твой друг и товарищ))')  # some features
+
+        else:
+            user_name = vk_bot.method('users.get', {'user_ids': user_id})
+            if 'Чуитс' in usr_txt:
+                write_msg(user_id, 'Чуитс, ' + (user_name[0]['first_name']))
+            elif 'Привет' in usr_txt:
+                write_msg(user_id, 'Привет, ' + (user_name[0]['first_name']))
+
+    # Меняем ts для следующего запроса
+    ts = long_poll['ts']
